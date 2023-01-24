386305db51c48bde43bb51274f58fff158efbb00
Kiarash Mokhtari <mokhtarikiarash@gmail.com>
2023-01-24 23:18:12 +0330

    added isValid func to solver

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,7 +1,61 @@
-# board is a 9*9 matrix
-def findEmpty (board):
-    for row in range(9):
-        for col in range(9):
-            if board[row][col] == 0:
-                return (row, col)
+# Initializing a board, empty cells are initialized with 0.
+def setBoard():
+    board = list()
+    sudokuBoard = '''200080300
+060070084
+030500209
+000105408
+000000000
+402706000
+301007040
+720040060
+004010003'''
+    rows = sudokuBoard.split('\n')
+    for row in rows:
+        column = list()
+        for character in row:
+            digit = int(character)
+            column.append(digit)
+        board.append(column)
+    return board
+
+# Find the first cell that is zero.
+def findEmpty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j) 
+                # row, col
     return None
+
+# Checking if the num is in the row or col or block.
+def isValid(board, num, pos):
+    
+    row, col = pos
+    # Checking the row:
+    for i in range(len(board[0])):
+        if board[row][i] == num and row != i:
+            return False
+
+    # Checking the col:
+    for i in range(len(board)):
+        if board[i][col] == num and col != i:
+            return False
+    
+    # Checking the block:
+    blockStart_x = 3 * (row // 3)
+    blockStart_y = 3 * (col // 3)
+
+    blockEnd_x = blockStart_x + 3
+    blockEnd_y = blockStart_y + 3
+
+    for i in range(blockStart_x, blockEnd_x):
+        for j in range(blockStart_y, blockEnd_y):
+            if board[i][j] == num and (row, col) != (i,j):
+                return False
+    return True 
+
+
+# Test
+board = setBoard()
+print(findEmpty(board))
